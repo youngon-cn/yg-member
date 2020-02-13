de9e967ff834c3a186681f6623f0b7e338c2d6dd
w-xuefeng <xuefeng@kcalb.wang>
2020-02-13 21:04:46 +0800

    fix: modify year to auto

--- src/elements/components/footer/index.js
+++ src/elements/components/footer/index.js
@@ -5,12 +5,14 @@ define('footer', class extends WeElement {
 
   backupNumber = '陇ICP备16002668号'
 
+  year = new Date().getFullYear()
+
   installed() {}
 
   render(props, data) {
     return (
       <div class="footer">
-        <span>Copyright © 2020 Youngon </span>
+        <span>Copyright © {this.year} Youngon </span>
         <a
           href="http://beian.miit.gov.cn"
           target="_blank"
